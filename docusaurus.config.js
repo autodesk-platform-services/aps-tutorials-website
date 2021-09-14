4dece500a5216d61a806157b0ecc1d4f05acc726
Petr Broz <ipetrbroz@gmail.com>
2021-09-14 09:53:53 +0200

    Fixed the "Edit on GitHub" links.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,7 +111,7 @@ module.exports = {
             {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/petrbroz/forge-samples-docs/edit/master/website/'
+                    editUrl: 'https://github.com/petrbroz/forge-samples-docs/edit/master/'
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css')
