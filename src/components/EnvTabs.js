7a4a0eaec407d6df468dea93a43326f42b643079
Petr Broz <petr.broz@autodesk.com>
2022-03-19 09:38:05 +0100

    Simplified the env. tabs.

---
+++ src/components/EnvTabs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+export default function EnvTabs({ NodeJsVsCode, DotNetVsCode, DotNetVs2022 }) {
+    return (
+        <Tabs groupId="development-environment">
+            <TabItem value="nodejs-vscode" label="Node.js & VSCode" default>
+                <NodeJsVsCode />
+            </TabItem>
+            <TabItem value="dotnet-vscode" label=".NET 6 & VSCode">
+                <DotNetVsCode />
+            </TabItem>
+            <TabItem value="dotnet-vs2022" label=".NET 6 & VS2022">
+                <DotNetVs2022 />
+            </TabItem>
+        </Tabs>
+    );
+}
