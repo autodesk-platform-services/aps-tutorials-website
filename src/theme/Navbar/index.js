da171ebd55760b2d1e4dd0c79b31e582d1f8511d
Petr Broz <petr.broz@autodesk.com>
2022-11-14 10:10:41 +0100

    Replaced docusaurus navbar with a 3rd party library...
    ... from the forge.autodesk.com portal engineering team.

---
+++ src/theme/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavbarLayout from '@theme/Navbar/Layout';
+import NavbarContent from '@theme/Navbar/Content';
+
+function loadNavbarScript() {
+  let s = document.createElement('script');
+  s.src = 'https://developer.static.autodesk.com/forgeunified/releases/current/adskf.common.entry.js';
+  s.dataset.resolveMenuUrl = 'https://forge.autodesk.com';
+  s.dataset.resolveFooterUrl = 'https://forge.autodesk.com';
+  document.head.appendChild(s);
+}
+
+loadNavbarScript();
+
+export default function Navbar() {
+  return (
+    <NavbarLayout>
+      {/* <NavbarContent /> */}
+      <div id="app-navbar" style={{width: '100%'}}></div>
+    </NavbarLayout>
+  );
+}
