df481ec78ec3b427ca52b70b86e028013047ac31
Petr Broz <petr.broz@autodesk.com>
2022-11-23 09:58:45 +0100

    Fixed the navbar error when used in SSR.

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -3,6 +3,10 @@ import NavbarLayout from '@theme/Navbar/Layout';
 import NavbarContent from '@theme/Navbar/Content';
 
 function loadNavbarScript() {
+  // Skip this code if we're in Server-Side Rendering mode
+  if (typeof window == 'undefined') {
+    return;
+  }
   let s = document.createElement('script');
   s.src = 'https://developer.static.autodesk.com/forgeunified/releases/current/adskf.common.entry.js';
   s.dataset.resolveMenuUrl = 'https://forge.autodesk.com';
